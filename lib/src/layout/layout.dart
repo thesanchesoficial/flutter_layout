34b6dd225565126dc760b1a7244e34d1434cf5fa
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2021-04-09 19:36:46 -0300

    adding route for widgets

--- lib/src/layout/layout.dart
+++ lib/src/layout/layout.dart
@@ -1,5 +1,6 @@
 library flutter_layout;
 
 export 'package:flutter_layout/src/layout/blank/blank.dart';
+export 'package:flutter_layout/src/layout/constructor_layout.dart';
 export 'package:flutter_layout/src/layout/grid/grid.dart';
 export 'package:flutter_layout/src/layout/layouter/layouter.dart';
