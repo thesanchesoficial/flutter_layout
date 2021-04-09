7e7f1f650c3b3fe828cac33de8c15139ba9c6eb3
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2021-04-09 18:21:31 -0300

    first commit

---
+++ lib/flutter_layout.dart
@@ -0,0 +1,3 @@
+library flutter_layout;
+
+export 'package:flutter_layout/src/layout/layout.dart';
---
+++ lib/src/layout/layout.dart
@@ -0,0 +1,5 @@
+library flutter_layout;
+
+export 'package:flutter_layout/src/layout/blank/blank.dart';
+export 'package:flutter_layout/src/layout/grid/grid.dart';
+export 'package:flutter_layout/src/layout/layouter/layouter.dart';
